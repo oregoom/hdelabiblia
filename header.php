845de86c55ddaedda87ff176c591686d02c28018
Oregoom <nivardo.choqque@gmail.com>
2023-11-17 14:26:41 -0500

    title

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
         
         <meta charset="<?php bloginfo( 'charset' ); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?php wp_title(); ?></title>
         
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
         
